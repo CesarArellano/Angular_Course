2ebc418b592b99dc6aa272d97b176a1c5ee7cf05
Cesar Arellano <cesarmauricio.arellano@gmail.com>
2026-07-06 14:21:27 -0600

    first commit

---
+++ typescript-intro/src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+import './topics/01-basic-types'
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vite.dev/guide/features.html" target="_blank">Documentation</a>
+`;
---
+++ typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,6 @@
+const name: string = 'César Arellano';
+let hpPoints: number | "Full" = 95;
+hpPoints = "Full";
+const isAlive: boolean = true;
+
+console.log({ name, hpPoints, isAlive });
