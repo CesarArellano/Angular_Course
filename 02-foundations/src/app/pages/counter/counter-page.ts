988317590c5c7e0db30916c9112167cd437c73f4
Cesar Arellano <cesarmauricio.arellano@gmail.com>
2026-07-07 12:19:05 -0600

    create html and css templates

--- 02-foundations/src/app/pages/counter/counter-page.ts
+++ 02-foundations/src/app/pages/counter/counter-page.ts
@@ -1,18 +1,17 @@
 import { Component } from "@angular/core";
 
 @Component({
-  template: `
-    <div>
-      <h1>Counter Page</h1>
-      <p>Counter: {{ counter }}</p>
-      <button (click)="increaseBy()">+1</button>
-    </div>
-  `
+  templateUrl: './counter-page.html',
+  styleUrls: ['./counter-page.css']
 })
 export class CounterPageComponent {
   counter = 10;
 
   increaseBy(value: number = 1) {
     this.counter += value;
+  }
+
+  reset() {
+    this.counter = 10;
   }
 }
