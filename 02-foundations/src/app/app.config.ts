9df03f77df6618dfa5dfcce0005356cdf2c34179
Cesar Arellano <cesarmauricio.arellano@gmail.com>
2026-07-09 16:08:07 -0600

    implement hashrouter

--- 02-foundations/src/app/app.config.ts
+++ 02-foundations/src/app/app.config.ts
@@ -2,10 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    // Hash location strategy for Netlify
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
   ]
 };
