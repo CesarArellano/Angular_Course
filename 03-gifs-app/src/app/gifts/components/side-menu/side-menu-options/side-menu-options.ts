51d388e55f28458ba9911945485341e70784d780
Cesar Arellano <cesarmauricio.arellano@gmail.com>
2026-07-10 14:13:56 -0600

    improve side menu with font-awesome package and new menu options

--- 03-gifs-app/src/app/gifts/components/side-menu/side-menu-options/side-menu-options.ts
+++ 03-gifs-app/src/app/gifts/components/side-menu/side-menu-options/side-menu-options.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+	icon: string;
+	label: string;
+	subLabel: string;
+	route: string;
+}
 
 @Component({
 	selector: 'gifs-side-menu-options',
-	imports: [],
+	imports: [RouterLinkActive, RouterLink],
 	templateUrl: './side-menu-options.html',
 })
-export class SideMenuOptionsComponent {}
+export class SideMenuOptionsComponent {
+	menuOptions: MenuOption[] = [
+		{
+			icon: 'fa-solid fa-chart-line',
+			label: 'Trending',
+			subLabel: 'Popular Gifs',
+			route: '/dashboard/trending',
+		},
+		{
+			icon: 'fa-solid fa-magnifying-glass',
+			label: 'Search',
+			subLabel: 'Search Gifs',
+			route: '/dashboard/search',
+		},
+	];
+}
