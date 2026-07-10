229c0a0946bd9aa399144d6052742ce2a9f0a5dc
Cesar Arellano <cesarmauricio.arellano@gmail.com>
2026-07-10 14:29:58 -0600

    add environments files and path alias

---
+++ 03-gifs-app/src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+	production: false,
+	companyName: 'Gifs App',
+	companySlogan: 'Handle your gifs with ease',
+
+	// API KEYS
+	// URLs
+};
---
+++ 03-gifs-app/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+	production: true,
+	companyName: 'Gifs App',
+	companySlogan: 'Handle your gifs with ease',
+
+	// API KEYS
+	// URLs
+};
