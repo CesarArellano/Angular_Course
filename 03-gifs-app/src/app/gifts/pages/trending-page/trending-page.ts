d1b78e277713ffe945db0140b24a86e34a608e22
Cesar Arellano <cesarmauricio.arellano@gmail.com>
2026-07-14 22:57:58 -0600

    call HTML reference

--- 03-gifs-app/src/app/gifts/pages/trending-page/trending-page.ts
+++ 03-gifs-app/src/app/gifts/pages/trending-page/trending-page.ts
@@ -1,13 +1,25 @@
-import { Component, computed, inject } from '@angular/core';
-import { ListComponent } from '../../components/list/list';
+import {
+	Component,
+	computed,
+	ElementRef,
+	inject,
+	viewChild,
+} from '@angular/core';
 import { GifsService } from '../../services/gifs.service';
 
 @Component({
 	selector: 'app-trending-page',
-	imports: [ListComponent],
+	// imports: [ListComponent],
 	templateUrl: './trending-page.html',
 })
 export default class TrendingPageComponent {
+	scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
+
 	gifsService = inject(GifsService);
 	gifs = computed(() => this.gifsService.trendingGifs());
+
+	onScroll(event: Event) {
+		const scrollDiv = this.scrollDivRef()?.nativeElement;
+		console.log(scrollDiv);
+	}
 }
