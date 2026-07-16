97062f12b8b5aff2569bae614df414eae895b7cd
Cesar Arellano <cesarmauricio.arellano@gmail.com>
2026-07-16 11:54:47 -0600

    implement routes and define the structure of the project for country-app

---
+++ 04-country-app/src/app/country/pages/by-capital-page/by-capital-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'country-by-capital-page',
+  imports: [],
+  templateUrl: './by-capital-page.html',
+})
+export class ByCapitalPageComponent {}
