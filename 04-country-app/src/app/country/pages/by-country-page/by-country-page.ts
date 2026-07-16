20f5246ce2ee84f45056339fe45a1aa34469935a
Cesar Arellano <cesarmauricio.arellano@gmail.com>
2026-07-16 12:26:41 -0600

    add new top-menu and routes

---
+++ 04-country-app/src/app/country/pages/by-country-page/by-country-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'country-by-country-page',
+  imports: [],
+  templateUrl: './by-country-page.html',
+})
+export class ByCountryPageComponent {}
---
+++ 04-country-app/src/app/country/pages/by-region-page/by-region-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'country-by-region-page',
+  imports: [],
+  templateUrl: './by-region-page.html',
+})
+export class ByRegionPageComponent {}
